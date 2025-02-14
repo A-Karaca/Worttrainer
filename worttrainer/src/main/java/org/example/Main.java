e0c943732ec5f585f4ce519dd206d9fbd982991e
A-Karaca <akaraca@student.tgm.ac.at>
2025-02-14 09:01:25 +0100

    gradle update

--- worttrainer/src/main/java/org/example/Main.java
+++ worttrainer/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Test test = new Test(1,2);
+        test.setNum1(4);
+        System.out.println(test.testMethod());
     }
 }
