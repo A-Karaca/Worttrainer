6575cf71461f6191013f8a32257d499bd3d5c60e
A-Karaca <akaraca@student.tgm.ac.at>
2025-02-14 11:11:05 +0100

    UML fixed, Spellingtrainer finished and testet

--- worttrainer/src/main/java/aKaraca/Main.java
+++ worttrainer/src/main/java/aKaraca/Main.java
@@ -5,7 +5,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SpellingTrainer rechtschreibtrainer = new SpellingTrainer();
+        SpellingTrainer spellingTrainer = new SpellingTrainer();
+        try {
+            spellingTrainer.selectItem(6);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(spellingTrainer.getStats());
+        try {
+            System.out.println(spellingTrainer.getCurrentItem()[0]);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            spellingTrainer.compareItem("Katze");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(spellingTrainer.getStats());
+
+
+        spellingTrainer.randomizeItem();
+        try {
+            System.out.println(spellingTrainer.getCurrentItem()[0]);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            spellingTrainer.compareItem(System.console().readLine());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(spellingTrainer.getStats());
 
     }
 }
--- worttrainer/src/main/java/aKaraca/SpellingTrainer.java
+++ worttrainer/src/main/java/aKaraca/SpellingTrainer.java
@@ -5,13 +5,15 @@ public class SpellingTrainer {
     private int selectedIndex;
     // 0: Correct, 1: Wrong, 2: Total
     private int[] playerStats;
+    private int listLength;
     /**
      * Contructor for the Rechtschreibtrainer class
      * @autor Ali Karaca
      * @version 14-02-2025
      */
-    public void spellingTrainer() {
-        wordList = new String[10][2];
+    public SpellingTrainer() {
+        listLength = 10;
+        wordList = new String[listLength][2];
         wordList[0][0] = "Haus";
         wordList[0][1] = "URLPlaceholder";
         wordList[1][0] = "Auto";
@@ -38,5 +40,65 @@ public class SpellingTrainer {
         playerStats[0] = 0;
         playerStats[1] = 0;
         playerStats[2] = 0;
+
+        selectedIndex = -1;
     }
+
+    /**
+     * Returns the Statistics of the player
+     * @return Statistics of the player as a String
+     */
+    public String getStats(){
+        return "Correct: " + playerStats[0] + " Wrong: " + playerStats[1] + " Total: " + playerStats[2];
+    }
+
+    /**
+     * Returns the Word and URL combination of the currently selected Item
+     * @return Word and URL combination as String Array
+     * @throws Exception if no item is selected
+     */
+    public String[] getCurrentItem() throws Exception {
+        if(selectedIndex == -1){
+            throw new Exception("No item selected");
+        }
+        return wordList[selectedIndex];
+    }
+
+    /**
+     * Compares the player input with the currently selected item then adds to the player stats
+     * @param playerInput The input of the player as String
+     * @throws Exception if no item is selected
+     */
+    public void compareItem(String playerInput) throws Exception {
+        if(selectedIndex == -1){
+            throw new Exception("No item selected");
+        }
+        if(wordList[selectedIndex][0].equals(playerInput)){
+            playerStats[0]++;
+        }else{
+            playerStats[1]++;
+        }
+        playerStats[2]++;
+    }
+
+    /**
+     * Selects the item with the given index, currently available: 0-9
+     * @param index The index of the item to be selected as int
+     * @throws Exception if the given index does not exist
+     */
+    public void selectItem(int index) throws Exception {
+        if(index < 0 || index > listLength-1){
+            throw new Exception("Index does not exist");
+        }
+        selectedIndex = index;
+    }
+
+    /**
+     * Randomly selects an item
+     */
+    public void randomizeItem() {
+        selectedIndex = (int) (Math.random() * wordList.length);
+    }
+
+
 }
